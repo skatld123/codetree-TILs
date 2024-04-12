b8d460d3a93ba53178ff87e8136234e9daf47861
nseungho9 <79092711+skatld123@users.noreply.github.com>
2024-04-12 11:05:07 +0900

    Title: 수업태도 평가, Time: 148ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240412/수업태도 평가/class-attitude-assessment.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().strip())
+
+students = {}
+for _ in range(n):
+    name, score = input().strip().split()
+    if name in students:
+        students[name] += int(score)
+    else:
+        students[name] = int(score)
+    if n == 1:
+        print(name)
+
+if len(students) > 1:
+    sorted(students, key=lambda x : x[1])
+    mim = 0
+    second = []
+    for idx, (k, v) in enumerate(students.items()):
+        if idx == 0: mim = v
+        if v > mim:
+            if second and second[0][1] != v:
+                break
+            second.append([k, v])
+
+    if not second or len(second) > 1:
+        print("Tie")
+    else:
+        print(second[0])
